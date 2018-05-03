05305ed5223ad2968747d3e2ee2c9b6f57f00b2a
niris <nirisix@gmail.com>
2018-05-03 18:45:52 +0800

    backup

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,7 +6,6 @@ import route from './route';
 
 const main = async () => {
     await prepare();
-
     const server = Hapi.server({
         host: '0.0.0.0',
         port: Number(process.env.PORT || 8000),
