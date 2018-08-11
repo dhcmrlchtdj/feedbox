120586a12ae55a5064b14bc6e1101d92ced52c53
niris <nirisix@gmail.com>
2018-08-11 11:48:34 +0800

    cleanup

--- server/src/controller/wakeup.ts
+++
@@ -1,4 +0,0 @@
-export const wakeup = {
-    auth: 'wakeupAuth',
-    handler: async (_req, h) => h.response('ok'),
-};
--- server/src/lib/auth/wakeup.ts
+++
@@ -1,8 +0,0 @@
-export default async (_req, username, password, _h) => {
-    return {
-        isValid:
-            username === process.env.WAKEUP_USERNAME &&
-            password === process.env.WAKEUP_PASSWORD,
-        credentials: {},
-    };
-};
