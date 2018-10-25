83172d18046b4aeb1e83ee66a484f2a0c39b0886
niris <nirisix@gmail.com>
2018-10-25 22:32:40 +0800

    update: send email

--- server/src/utils/send-email.ts
+++ server/src/utils/send-email.ts
@@ -1,24 +1,19 @@
 import * as Mailgun from "mailgun-js";
 
-const mailgun = new Mailgun({
+const mg = new Mailgun({
     apiKey: process.env.MAILGUN_API_KEY as string,
     domain: process.env.MAILGUN_DOMAIN as string,
 });
-const messages = mailgun.messages();
 
-const sender = process.env.MAILGUN_SENDER;
-
-const send = async (receiver: string, subject: string, text: string) => {
+const send = async (addr: string, subject: string, text: string) => {
     const data = {
-        from: sender,
-        to: receiver,
+        from: process.env.MAILGUN_FROM,
+        to: addr,
         subject,
         text,
     };
-
-    const resp = await messages.send(data);
-
-    return resp;
+    const reply = await mg.messages().send(data);
+    return reply;
 };
 
 export default send;
