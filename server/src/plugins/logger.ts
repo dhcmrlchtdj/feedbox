295150a9364792467ca4d3dfdea8875707941f45
niris <nirisix@gmail.com>
2018-10-26 00:25:09 +0800

    update: logger

--- server/src/plugins/logger.ts
+++ server/src/plugins/logger.ts
@@ -1,15 +1,37 @@
 import * as Good from "good";
 import * as GoodConsole from "good-console";
+import * as GoodSqueeze from "good-squeeze";
 
 const logger = {
     plugin: Good,
     options: {
         ops: false,
+        includes: {
+            request: ["headers"],
+            response: ["headers"],
+        },
         reporters: {
-            console: [
+            simple: [
+                {
+                    module: GoodSqueeze.Squeeze,
+                    args: [
+                        {
+                            log: "*",
+                            error: "*",
+                            request: "*",
+                            response: "*",
+                        },
+                    ],
+                },
                 {
                     module: GoodConsole,
                     args: [{ format: "YYYY-MM-DDTHH:mm:ss.SSS" }],
+                },
+                "stdout",
+            ],
+            detail: [
+                {
+                    module: GoodSqueeze.SafeJson,
                 },
                 "stdout",
             ],
