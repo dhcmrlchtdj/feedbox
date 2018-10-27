9ff8729764ffa80c1a7aab970dc8ce0ffdfaa2e3
niris <nirisix@gmail.com>
2018-10-27 11:06:15 +0800

    fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ const main = async (): Promise<void> => {
     await prepare();
 
     const server = Hapi.server({
-        host: "localhost",
+        host: "0.0.0.0",
         port: Number(process.env.PORT || 8000),
     });
 
