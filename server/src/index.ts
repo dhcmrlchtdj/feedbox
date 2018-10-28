65927c30c97cbd71880637fb3c04666d89e8a653
niris <nirisix@gmail.com>
2018-10-28 19:45:20 +0800

    update: cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,7 @@ const main = async (): Promise<void> => {
     const server = Hapi.server({
         host: "0.0.0.0",
         port: Number(process.env.PORT || 8000),
+        routes: { cors: { credentials: true } },
     });
 
     await plugins(server);
