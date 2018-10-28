1b8cc57c71097b6406fc16e91cb8d5b1dae78ed3
niris <nirisix@gmail.com>
2018-10-28 21:09:36 +0800

    add purecss

--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,10 @@
-import "spectre.css";
+import "purecss";
 import App from "./components/app.svelte";
 
 const app = new App({
     target: document.body,
     data: {
-        name: "world",
+        name: "w",
     },
 });
 
