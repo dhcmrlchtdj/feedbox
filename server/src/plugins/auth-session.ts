e07b9c41e4d81227ad049a6e3945705ae4cc917e
h11 <nirisix@gmail.com>
2019-04-06 13:44:29 +0800

    chore: upgrade dep

--- server/src/plugins/auth-session.ts
+++ server/src/plugins/auth-session.ts
@@ -14,14 +14,16 @@ export default async (server): Promise<void> => {
     await server.register(authCookie)
 
     server.auth.strategy('session', 'cookie', {
-        password: process.env.COOKIE_SECRET,
-        cookie: 'token',
-        path: '/api',
-        ttl: 7 * 24 * 60 * 60 * 1000,
-        clearInvalid: true,
-        isHttpOnly: true,
-        isSameSite: process.env.API === process.env.SITE ? 'Strict' : false,
-        isSecure: process.env.NODE_ENV === 'production',
+        cookie: {
+            name: 'token',
+            password: process.env.COOKIE_SECRET,
+            ttl: 7 * 24 * 60 * 60 * 1000,
+            path: '/api',
+            clearInvalid: true,
+            isSameSite: process.env.API === process.env.SITE ? 'Strict' : false,
+            isSecure: process.env.NODE_ENV === 'production',
+            isHttpOnly: true,
+        },
         validateFunc: validate,
     })
 }
