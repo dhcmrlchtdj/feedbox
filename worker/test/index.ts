c9fcea5e1c149940fbf3e26ee9017f02d084bcef
h11 <nirisix@gmail.com>
2019-12-23 19:47:07 +0800

    worker | test

--- worker/test/index.ts
+++ worker/test/index.ts
@@ -1,19 +1,13 @@
-const assert = require('assert')
 const fs = require('fs')
 const path = require('path')
 const Cloudworker = require('@dollarshaveclub/cloudworker')
 
-const readStr = async (p: string) => (await fs.promises.readFile(p)).toString()
 const scriptPath = path.resolve(__dirname, '../bundle/index.js')
+const script = fs.readFileSync(scriptPath).toString()
 
-const test = async () => {
-    const script = await readStr(scriptPath)
+test('rewrite host', async () => {
     const cw = new Cloudworker(script)
     const req = new Cloudworker.Request('http://feedbox.h11.io/api/v1/user')
-    const res = await cw.dispatch(req)
-
-    assert(res.url === 'http://localhost:8000/api/v1/user')
-    assert(res.status === 401)
-}
-
-test()
+    const resp = await cw.dispatch(req)
+    expect(resp.url).toEqual('http://localhost:8000/api/v1/user')
+})
