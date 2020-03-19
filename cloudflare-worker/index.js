9e2aa596a3a8d0ab9484c7946c9582a07f8ce402
h11 <nirisix@gmail.com>
2020-03-19 15:11:42 +0800

    fix: add worker

---
+++ cloudflare-worker/index.js
@@ -0,0 +1,18 @@
+async function handle(request) {
+    const apiHost = 'fbox.herokuapp.com'
+    const url = new URL(request.url)
+    url.host = apiHost
+    const init = {
+        method: request.method,
+        headers: request.headers,
+        body: request.body,
+        redirect: 'manual',
+    }
+    const req = new Request(url, init)
+    req.headers.set('host', apiHost)
+    return fetch(req)
+}
+
+addEventListener('fetch', async event => {
+    event.respondWith(handle(event.request))
+})
