dad885d2eb458d59bdf3ab45e2168fc8f2ecb8d3
h11 <nirisix@gmail.com>
2020-03-20 10:51:15 +0800

    Revert "cloudflare worker cache"
    This reverts commit 079f6184f9ae6a903780b5507f5716d5dce40631.

--- cloudflare-worker/index.js
+++ cloudflare-worker/index.js
@@ -1,46 +1,18 @@
-const apiHost = 'fbox.herokuapp.com'
-
-const redirect = async event => {
-    const request = event.request
+async function handle(request) {
+    const apiHost = 'fbox.herokuapp.com'
     const url = new URL(request.url)
     url.host = apiHost
-    const req = new Request(url, {
+    const init = {
         method: request.method,
         headers: request.headers,
         body: request.body,
         redirect: 'manual',
-    })
+    }
+    const req = new Request(url, init)
     req.headers.set('host', apiHost)
     return fetch(req)
 }
 
-const redirectWithCache = async event => {
-    const request = event.request
-
-    const cache = caches.default
-    const m = await cache.match(request)
-    if (m) return m
-
-    const resp = await redirect(event)
-    if (resp.ok) {
-        const etag = resp.headers.get('etag')
-        if (etag) {
-            event.waitUntil(cache.put(request, resp.clone()))
-        }
-    }
-
-    return resp
-}
-
-const handle = event => {
-    const method = event.request.method.toUpperCase()
-    if (method === 'GET' || method === 'HEAD') {
-        return redirectWithCache(event)
-    } else {
-        return redirect(event)
-    }
-}
-
 addEventListener('fetch', async event => {
-    event.respondWith(handle(event))
+    event.respondWith(handle(event.request))
 })
