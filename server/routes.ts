c58c2006a65a7845458f030083d077309f6388cd
h11 <nirisix@gmail.com>
2020-03-22 15:59:20 +0800

    #51 | hapi cache control

--- server/routes.ts
+++ server/routes.ts
@@ -1,11 +1,7 @@
 import * as Feed from './controllers/feed'
 import * as User from './controllers/user'
 import * as Helper from './controllers/helper'
-
-const icon = Buffer.from(
-    'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABmklEQVR4AcWWgUeDQRjGD6iogFgCooCiP6ZWf0QIomq7NygmAFWr/pigJSEChNik7q5Ai7bwdK/vSF8tt6/b3cOP+Yz3vXvvnnuEr/BC4zDVFWh5Ak1XFm3pOjR/g6I6DC3DrI+JUIKhWVv0zPJmgR/8XzqF3p4pXri1NgJF+9Dyw4JiUBeGarinYdGPuHNoeWtBIBowm5OexSsL2UwlAtOConmPlYcunmtCU0n8Jp4TNN0MsLiDrvl8ibzcgUMUlNz5edXcaY/E67dRuHuOqBg6+HI4ZzJxoTbYMZ29Ig3VsmBvT9aAkscie1gkEtHgHTAJG1ACSnbSNUDv6RtIPoL0h1BRPd01pCPBGS6hES3yCEYt7RRWzLUFiwNkgu2v55NQN6IFd6BoOp8JahF3YK9HJJONCLO/wN3qUI9QSiVLc4ANPOB5a0r8JY7OnF7DF6cmnipzwkd43JiAludBt91lQG/xnDi9/scjsoeOdt3Mi4k75wDZXyPU5rTjrloQOcesltm/YeiSXzJeIcO/+RuMPISmJedwXvoEIKUq7CctWTEAAAAASUVORK5CYII',
-    'base64',
-)
+import { staticFiles } from './controllers/static'
 
 export const routes = [
     { path: '/api/v1/feeds', method: 'get', options: Feed.list },
@@ -24,26 +20,5 @@ export const routes = [
         options: Helper.feedPreview,
     },
 
-    {
-        path: '/favicon.ico',
-        method: 'get',
-        options: {
-            auth: false,
-            async handler(_request, h) {
-                return h
-                    .response(icon)
-                    .type('image/png')
-                    .etag('favicon-v1')
-            },
-        },
-    },
-
-    {
-        path: '/{param?}',
-        method: 'get',
-        options: {
-            auth: false,
-            handler: { directory: { path: '.', index: 'index.html' } },
-        },
-    },
+    ...staticFiles,
 ].filter(Boolean)
