35ff5438929d1ac0fcf4661d3975f6b7a260896f
h11 <nirisix@gmail.com>
2020-03-22 12:29:12 +0800

    disable cf worker cache

--- cloudflare-worker/index.ts
+++ cloudflare-worker/index.ts
@@ -3,14 +3,14 @@
 import {} from '@cloudflare/workers-types'
 import { Router } from '../util/router'
 
-// https://github.com/cloudflare/workers-types/issues/8
-interface Caches {
-    default: {
-        put(request: Request | string, response: Response): Promise<undefined>
-        match(request: Request | string): Promise<Response | undefined>
-    }
-}
-declare const caches: Caches
+// // https://github.com/cloudflare/workers-types/issues/8
+// interface Caches {
+//     default: {
+//         put(request: Request | string, response: Response): Promise<undefined>
+//         match(request: Request | string): Promise<Response | undefined>
+//     }
+// }
+// declare const caches: Caches
 
 const apiHost = 'fbox.herokuapp.com'
 
@@ -30,27 +30,28 @@ const direct = async (event) => {
     return fetch(req)
 }
 
-const withCache = async (event) => {
-    const cache = await caches.default
-    const request = event.request
-    const m = await cache.match(request)
-    if (m) return m
-
-    const resp = await direct(event)
-    if (resp.status === 200) {
-        event.waitUntil(cache.put(request, resp.clone()))
-    }
-
-    return resp
-}
+// const withCache = async (event) => {
+//     const cache = await caches.default
+//     const request = event.request
+//     const m = await cache.match(request)
+//     if (m) return m
+//
+//     const resp = await direct(event)
+//     if (resp.status === 200) {
+//         event.waitUntil(cache.put(request, resp.clone()))
+//     }
+//
+//     return resp
+// }
 
 const router = new Router()
-router.fallback(direct).get('/:file', async (event, params) => {
-    const file = params.get('file')!
-    if (file.startsWith('sw.js')) return direct(event)
-
-    return withCache(event)
-})
+router.fallback(direct)
+// .get('/:file', async (event, params) => {
+//     const file = params.get('file')!
+//     if (file.startsWith('sw.js')) return direct(event)
+//
+//     return withCache(event)
+// })
 
 addEventListener('fetch', (event) => {
     const resp = router.route(event)
