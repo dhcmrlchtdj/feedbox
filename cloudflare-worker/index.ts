9e95561e43d9bf440d52aafce046dc8d37e04f4c
h11 <nirisix@gmail.com>
2020-03-22 18:03:34 +0800

    cleanup worker

--- cloudflare-worker/index.ts
+++ cloudflare-worker/index.ts
@@ -1,20 +1,10 @@
 /// <reference lib="webworker" />
 
 import {} from '@cloudflare/workers-types'
-import { Router } from '../util/router'
-
-// // https://github.com/cloudflare/workers-types/issues/8
-// interface Caches {
-//     default: {
-//         put(request: Request | string, response: Response): Promise<undefined>
-//         match(request: Request | string): Promise<Response | undefined>
-//     }
-// }
-// declare const caches: Caches
 
 const apiHost = 'fbox.herokuapp.com'
 
-const direct = async (event) => {
+const handle = (event: FetchEvent) => {
     const request = event.request
 
     const url = new URL(request.url)
@@ -30,30 +20,7 @@ const direct = async (event) => {
     return fetch(req)
 }
 
-// const withCache = async (event) => {
-//     const cache = await caches.default
-//     const request = event.request
-//     const m = await cache.match(request)
-//     if (m) return m
-//
-//     const resp = await direct(event)
-//     if (resp.status === 200) {
-//         event.waitUntil(cache.put(request, resp.clone()))
-//     }
-//
-//     return resp
-// }
-
-const router = new Router()
-router.fallback(direct)
-// .get('/:file', async (event, params) => {
-//     const file = params.get('file')!
-//     if (file.startsWith('sw.js')) return direct(event)
-//
-//     return withCache(event)
-// })
-
 addEventListener('fetch', (event) => {
-    const resp = router.route(event)
+    const resp = handle(event)
     event.respondWith(resp)
 })
