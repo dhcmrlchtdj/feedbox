82aceb4ee9d3ef05be7700b278ae114e5eba671b
h11 <me@h11.io>
2020-05-04 11:45:51 +0800

    move from sentry to rollbar

--- pkg/backend/server/plugins/error-reporter.ts
+++ pkg/backend/server/plugins/error-reporter.ts
@@ -1,9 +1,27 @@
-import hapiSentry from 'hapi-sentry'
-import { Sentry } from '../utils/error-reporter'
+import { rollbar } from '../utils/error-reporter'
 
 export const errorReporter = {
-    plugin: hapiSentry,
-    options: {
-        client: Sentry,
+    options: {},
+    plugin: {
+        name: 'errorReporter',
+        register: async (server, _options) => {
+            const preResponse = (request, h) => {
+                const resp = request.response
+                if (resp.isBoom) {
+                    const error =
+                        resp instanceof Error ? resp : `Error: ${resp}`
+                    if (resp.output.statusCode >= 500) {
+                        rollbar.error(error, request)
+                    } else if (resp.output.statusCode >= 400) {
+                        rollbar.info(error, request)
+                    } else {
+                        rollbar.debug(error, request)
+                    }
+                }
+                return h.continue
+            }
+            server.ext('onPreResponse', preResponse)
+            // server.expose('reporter', rollbar)
+        },
     },
 }
--- pkg/backend/server/utils/error-reporter.ts
+++ pkg/backend/server/utils/error-reporter.ts
@@ -1,24 +1,19 @@
-import * as Sentry from '@sentry/node'
+import Rollbar from 'rollbar'
 
-Sentry.init({
-    dsn: process.env.SENTRY_DSN,
+export const rollbar = new Rollbar({
+    accessToken: process.env.ROLLBAR_TOKEN!,
     enabled: process.env.NODE_ENV === 'production',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    reportLevel: 'debug'
 })
-
-export { Sentry }
 
 // https://github.com/getsentry/sentry-javascript/issues/1781#issuecomment-444510327
 export const report = {
     err: (err: Error, tags: Record<string, string> = {}) => {
-        Sentry.withScope((scope) => {
-            scope.setTags(tags)
-            Sentry.captureException(err)
-        })
+        rollbar.error(err, tags)
     },
     log: (msg: string, tags: Record<string, string> = {}) => {
-        Sentry.withScope((scope) => {
-            scope.setTags(tags)
-            Sentry.captureMessage(msg)
-        })
+        rollbar.log(msg, tags)
     },
 }
