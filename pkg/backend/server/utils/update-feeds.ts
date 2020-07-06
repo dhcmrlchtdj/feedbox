4ac64304898ac618f08ea49b1cf921c23f1577ec
h11 <me@h11.io>
2020-07-06 09:49:56 +0800

    fix: split get new feeds

--- pkg/backend/server/utils/update-feeds.ts
+++ pkg/backend/server/utils/update-feeds.ts
@@ -1,7 +1,6 @@
 import { model } from '../models'
+import { getNewFeeds } from './get-new-feeds'
 import type { Feed, GithubUser, TelegramUser } from '../models'
-import { fetchFeed } from './fetch-feed'
-import { parseFeed } from './parse-feed'
 import type { FeedItem } from './parse-feed'
 import { sendEmail } from './send-email'
 import { telegramClient } from '../telegram/client'
@@ -9,7 +8,7 @@ import { extractSite } from './extract-site'
 import { Channel } from './sync'
 
 export const updateFeeds = async () => {
-    const chFeed = new Channel<Feed>(200)
+    const chFeed = new Channel<Feed>(300)
     const chFeedItem = new Channel<[Feed, FeedItem]>(20)
     const chEmail = new Channel<[Feed, FeedItem, GithubUser[]]>(20)
     const chTelegram = new Channel<[FeedItem, TelegramUser[]]>(20)
@@ -21,37 +20,13 @@ export const updateFeeds = async () => {
     // feed => feedItem
     chFeed
         .onReceive(10, async (feed) => {
-            const url = feed.url
-            const resp = await fetchFeed(url)
+            const resp = await getNewFeeds(feed)
             if (resp.isNone) return
-            const items = await parseFeed(url, resp.getExn())
-            if (items.length === 0) return
 
-            const oldLinks = await model.getLinks(feed.id)
-            const linkSet = new Set(oldLinks)
-            const newLinks: string[] = []
-            for (const item of items) {
-                const link = item.origlink || item.link || item.guid
-                if (linkSet.has(link)) continue
-                linkSet.add(link)
-                newLinks.push(link)
+            const { updated, newLinks, newItems } = resp.getExn()
+            await model.addNewFeeds(feed.id, newLinks, updated)
+            for (const item of newItems) {
                 await chFeedItem.send([feed, item])
-            }
-
-            if (newLinks.length > 0) {
-                const updated = (() => {
-                    const first = items[0]
-                    const date = first.date || first.meta.date
-                    if (
-                        date instanceof Date &&
-                        !Number.isNaN(date.getTime()) // Invalid Date
-                    ) {
-                        return date
-                    } else {
-                        return new Date()
-                    }
-                })()
-                await model.addNewFeeds(feed.id, newLinks, updated)
             }
         })
         .then(() => chFeedItem.close())
