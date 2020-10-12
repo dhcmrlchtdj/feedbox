dabb624dd001e54613f7620ea2984bd3846587e9
h11 <me@h11.io>
2020-10-12 09:01:03 +0800

    fix etag compare algorithm

--- server/middleware/etag/etag.go
+++ server/middleware/etag/etag.go
@@ -8,14 +8,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// https://www.w3.org/Protocols/HTTP/1.1/rfc2616bis/issues/#i71
-func match(clientEtag string, serverEtag string) bool {
-	if strings.HasPrefix(clientEtag, "W/") {
-		return clientEtag[2:] == serverEtag[2:]
-	}
-	return clientEtag == serverEtag[2:]
-}
-
 func New() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		err := c.Next()
@@ -38,10 +30,11 @@ func New() fiber.Handler {
 
 		// Generate ETag for response
 		sum := xxhash.Sum64(body)
-		serverEtag := fmt.Sprintf("W/\"%016x\"", sum)
+		serverEtag := fmt.Sprintf("W/\"%d-%016x\"", len(body), sum)
 		c.Set("etag", serverEtag)
 
-		if match(clientEtag, serverEtag) {
+		// https://www.w3.org/Protocols/HTTP/1.1/rfc2616bis/issues/#i71
+		if strings.Contains(clientEtag, serverEtag[2:]) {
 			c.Status(fiber.StatusNotModified)
 			return c.Send(nil)
 		}
