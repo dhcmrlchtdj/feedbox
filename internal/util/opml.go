cb0b03a42362c583ee7a05d95494a246a27b4d3c
h11 <me@h11.io>
2022-02-08 20:49:08 +0800

    fix newline

--- internal/util/opml.go
+++ internal/util/opml.go
@@ -45,8 +45,9 @@ func BuildOPML(urls []string) []byte {
 	for _, url := range urls {
 		text := html.EscapeString(ExtractSiteName(url))
 		xmlURL := html.EscapeString(url)
-		outline := fmt.Sprintf(`<outline type="rss" text="%s" xmlUrl="%s"/>\n`, text, xmlURL)
+		outline := fmt.Sprintf(`<outline type="rss" text="%s" xmlUrl="%s"/>`, text, xmlURL)
 		b.WriteString(outline)
+		b.WriteByte('\n')
 	}
 
 	b.WriteString("</body>\n")
