15d3ab3eefb3613b9c0266134385bf656acd21a8
h11 <me@h11.io>
2022-10-29 21:17:33 +0800

    chore(style): retab

--- migration/postgresql/1_link-col-type.down.sql
+++ migration/postgresql/1_link-col-type.down.sql
@@ -3,24 +3,24 @@ BEGIN;
 ALTER TABLE feeds ADD COLUMN links JSONB DEFAULT '[]'::jsonb;
 
 CREATE PROCEDURE array2json() AS $$
-    DECLARE
-        fid int;
-    BEGIN
-        FOR fid IN
-            SELECT id FROM feeds
-        LOOP
-            UPDATE feeds SET links =
-            (SELECT json_agg(x)
-                FROM feeds, unnest(feeds.link) AS x
-                WHERE id = fid)
-            WHERE id = fid;
-        END LOOP;
-    END;
+	DECLARE
+		fid INT;
+	BEGIN
+		FOR fid IN
+		SELECT id FROM feeds
+		LOOP
+			UPDATE feeds SET links =
+			(SELECT JSON_AGG(x)
+				FROM feeds, UNNEST(feeds.link) AS x
+				WHERE id = fid)
+			WHERE id = fid;
+		END LOOP;
+	END;
 $$ LANGUAGE plpgsql;
 CALL array2json();
 DROP PROCEDURE array2json;
 
-UPDATE feeds SET links = '[]'::jsonb WHERE links is NULL;
+UPDATE feeds SET links = '[]'::jsonb WHERE links IS NULL;
 ALTER TABLE feeds ALTER COLUMN links SET NOT NULL;
 
 ALTER TABLE feeds DROP COLUMN link;
