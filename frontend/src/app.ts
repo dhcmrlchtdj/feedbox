1447da5aabe79489f07198864b856551322b5e6b
h11 <me@h11.io>
2022-11-06 21:15:10 +0800

    fe: add sw state to localstorage

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -14,12 +14,51 @@ const unregisterServiceWorker = async () => {
 	await Promise.all(cacheKeys.map((key) => caches.delete(key)))
 }
 
+class LocalState {
+	getLocal(key: string): string | null {
+		try {
+			const val = localStorage.getItem(key)
+			return val
+		} catch (err) {
+			console.error(err)
+			return null
+		}
+	}
+	setLocal(key: string, value: string) {
+		try {
+			localStorage.setItem(key, value)
+		} catch (err) {
+			console.error(err)
+		}
+	}
+
+	getQuery(key: string): string | null {
+		const query = new URLSearchParams(location.search)
+		return query.get(key)
+	}
+
+	getQueryOrLocal(
+		queryKey: string,
+		localKey: string,
+		defaultValue: string,
+	): string {
+		const query = this.getQuery(queryKey)
+		if (query) return query
+		const local = this.getLocal(localKey)
+		if (local) return local
+		return defaultValue
+	}
+}
+
 if (navigator.serviceWorker) {
-	const query = new URLSearchParams(location.search)
-	const sw = query.get("sw")
-	if (sw === "0") {
+	const localState = new LocalState()
+	const swEnable = localState.getQueryOrLocal("sw", "sw-enable", "true")
+
+	if (swEnable === "false" || swEnable === "0") {
 		unregisterServiceWorker()
+		localState.setLocal("sw-enable", "false")
 	} else {
 		navigator.serviceWorker.register("/sw.js")
+		localState.setLocal("sw-enable", "true")
 	}
 }
