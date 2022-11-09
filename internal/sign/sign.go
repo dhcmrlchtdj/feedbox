fb0354ec62c0aea80e97c0e5d5e602e34ebb2b89
h11 <me@h11.io>
2022-11-09 22:31:53 +0800

    set argon2 thread to 4

--- internal/sign/sign.go
+++ internal/sign/sign.go
@@ -28,7 +28,9 @@ func New(password string) (*Sign, error) {
 	// https://datatracker.ietf.org/doc/rfc9106/
 	// salt, 16 bytes is RECOMMENDED for password hashing
 	// The Argon2id variant with t=3 and 64 MiB memory is the SECOND RECOMMENDED option
-	key := argon2.IDKey([]byte(password), salt, 3, 64*1024, 1, 32)
+	// Argon2id with t=1 iteration, p=4 lanes, m=2 GiB of RAM, 128-bit salt, and 256-bit tag size.
+	// Argon2id with t=3 iterations, p=4 lanes, m=64 MiB of RAM, 128-bit salt, and 256-bit tag size.
+	key := argon2.IDKey([]byte(password), salt, 3, 64*1024, 4, 32)
 	aead, err := chacha20poly1305.NewX(key)
 	if err != nil {
 		return nil, err
