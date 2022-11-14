bb32c64814804830ce462f43495193d6c507a07d
h11 <me@h11.io>
2022-11-14 20:38:29 +0800

    graceful shutdown

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/signal"
 	"strconv"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/golang-migrate/migrate/v4"
@@ -41,35 +44,36 @@ func main() {
 	}
 }
 
+///
+
 func startServerAndWorker() {
 	initEnv()
 	initLogger()
 	initDatabase()
 	defer global.Database.Close()
-	initMailgun()
+	initEmail()
 	initTelegram()
 	initSign()
 
-	// start worker in background
-	ticker := time.NewTicker(time.Minute * 10)
+	quit := quitSignal()
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
 	go func() {
-		for t := range ticker.C {
-			if t.Minute() >= 50 {
-				worker.Start()
-			}
-		}
+		defer wg.Done()
+		runWorker(quit)
 	}()
 
-	// start server
-	util.CheckEnvs("GITHUB_CLIENT_ID", "GITHUB_CLIENT_SECRET")
-	util.CheckEnvs("HOST", "PORT", "TELEGRAM_WEBHOOK_PATH", "WORKER_TOKEN")
-	app := server.Create()
-	host := os.Getenv("HOST") + ":" + os.Getenv("PORT")
-	url := "http://" + host + os.Getenv("SERVER_SUB_DIR")
-	log.Info().Str("module", "app").Str("url", url).Msg("app started")
-	if err := app.Listen(host); err != nil {
-		panic(err)
-	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		runServer(quit)
+	}()
+
+	wg.Wait()
+
+	log.Info().Str("module", "app").Msg("app stopped")
 }
 
 func startServer() {
@@ -77,87 +81,23 @@ func startServer() {
 	initLogger()
 	initDatabase()
 	defer global.Database.Close()
-	initMailgun()
+	initEmail()
 	initTelegram()
 	initSign()
 
-	util.CheckEnvs("GITHUB_CLIENT_ID", "GITHUB_CLIENT_SECRET")
-	util.CheckEnvs("HOST", "PORT", "TELEGRAM_WEBHOOK_PATH", "WORKER_TOKEN")
-	app := server.Create()
-	host := os.Getenv("HOST") + ":" + os.Getenv("PORT")
-	url := "http://" + host + os.Getenv("SERVER_SUB_DIR")
-	log.Info().Str("module", "app").Str("url", url).Msg("app started")
-	if err := app.Listen(host); err != nil {
-		panic(err)
-	}
+	quit := quitSignal()
+	runServer(quit)
+	log.Info().Str("module", "app").Msg("app stopped")
 }
 
 func startWorker() {
 	initEnv()
 	initLogger()
 	initDatabase()
-	defer global.Database.Close()
+	initEmail()
 	initTelegram()
 
 	worker.Start()
-}
-
-func initEnv() {
-	if os.Getenv("ENV") != "prod" {
-		if err := godotenv.Load("./dotenv"); err != nil {
-			panic(err)
-		}
-	}
-	util.CheckEnvs("ENV")
-}
-
-func initLogger() {
-	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack // nolint:reassign
-	// log.Logger = log.Output(os.Stdout)
-}
-
-func initDatabase() {
-	util.CheckEnvs("DATABASE_URL")
-	db, err := database.New(os.Getenv("DATABASE_URL"), &log.Logger)
-	if err != nil {
-		panic(err)
-	}
-	global.Database = db
-}
-
-func initMailgun() {
-	if os.Getenv("ENV") == "prod" {
-		util.CheckEnvs("MAILGUN_DOMAIN", "MAILGUN_API_KEY", "MAILGUN_FROM")
-		global.Email = email.NewMailgun(
-			os.Getenv("MAILGUN_DOMAIN"),
-			os.Getenv("MAILGUN_API_KEY"),
-			os.Getenv("MAILGUN_FROM"),
-		)
-	} else {
-		global.Email = email.NewDryRun()
-	}
-}
-
-func initTelegram() {
-	if os.Getenv("ENV") == "prod" {
-		util.CheckEnvs("SERVER")
-		util.CheckEnvs("TELEGRAM_BOT_NAME", "TELEGRAM_BOT_TOKEN")
-		global.Telegram = telegram.NewHTTPClient(
-			os.Getenv("TELEGRAM_BOT_NAME"),
-			os.Getenv("TELEGRAM_BOT_TOKEN"),
-		)
-	} else {
-		global.Telegram = telegram.NewDryRun()
-	}
-}
-
-func initSign() {
-	util.CheckEnvs("COOKIE_SECRET")
-	s, err := sign.New(os.Getenv("COOKIE_SECRET"))
-	if err != nil {
-		panic(err)
-	}
-	global.Sign = s
 }
 
 func startMigration() {
@@ -217,4 +157,116 @@ func startMigration() {
 	default:
 		printUsage(m)
 	}
+}
+
+///
+
+func runServer(quit <-chan struct{}) {
+	util.CheckEnvs("GITHUB_CLIENT_ID", "GITHUB_CLIENT_SECRET")
+	util.CheckEnvs("HOST", "PORT", "TELEGRAM_WEBHOOK_PATH", "WORKER_TOKEN")
+
+	app := server.Create()
+	go func() {
+		<-quit
+		err := app.Shutdown()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	host := os.Getenv("HOST") + ":" + os.Getenv("PORT")
+	url := "http://" + host + os.Getenv("SERVER_SUB_DIR")
+	log.Info().Str("module", "app").Str("url", url).Msg("app started")
+	err := app.Listen(host)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func runWorker(quit <-chan struct{}) {
+	ticker := time.NewTicker(time.Minute * 10)
+	for {
+		select {
+		case t := <-ticker.C:
+			if t.Minute() >= 50 {
+				worker.Start()
+			}
+		case <-quit:
+			ticker.Stop()
+			return
+		}
+	}
+}
+
+///
+
+func initEnv() {
+	if os.Getenv("ENV") != "prod" {
+		if err := godotenv.Load("./dotenv"); err != nil {
+			panic(err)
+		}
+	}
+	util.CheckEnvs("ENV")
+}
+
+func initLogger() {
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack // nolint:reassign
+}
+
+func initDatabase() {
+	util.CheckEnvs("DATABASE_URL")
+	db, err := database.New(os.Getenv("DATABASE_URL"), &log.Logger)
+	if err != nil {
+		panic(err)
+	}
+	global.Database = db
+}
+
+func initEmail() {
+	if os.Getenv("ENV") == "prod" {
+		util.CheckEnvs("MAILGUN_DOMAIN", "MAILGUN_API_KEY", "MAILGUN_FROM")
+		global.Email = email.NewMailgun(
+			os.Getenv("MAILGUN_DOMAIN"),
+			os.Getenv("MAILGUN_API_KEY"),
+			os.Getenv("MAILGUN_FROM"),
+		)
+	} else {
+		global.Email = email.NewDryRun()
+	}
+}
+
+func initTelegram() {
+	if os.Getenv("ENV") == "prod" {
+		util.CheckEnvs("SERVER")
+		util.CheckEnvs("TELEGRAM_BOT_NAME", "TELEGRAM_BOT_TOKEN")
+		global.Telegram = telegram.NewHTTPClient(
+			os.Getenv("TELEGRAM_BOT_NAME"),
+			os.Getenv("TELEGRAM_BOT_TOKEN"),
+		)
+	} else {
+		global.Telegram = telegram.NewDryRun()
+	}
+}
+
+func initSign() {
+	util.CheckEnvs("COOKIE_SECRET")
+	s, err := sign.New(os.Getenv("COOKIE_SECRET"))
+	if err != nil {
+		panic(err)
+	}
+	global.Sign = s
+}
+
+///
+
+func quitSignal() <-chan struct{} {
+	quit := make(chan struct{})
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		sig := <-c
+		log.Info().Str("module", "app").Str("signal", sig.String()).Msg("app stopping")
+		close(quit)
+	}()
+	return quit
 }
