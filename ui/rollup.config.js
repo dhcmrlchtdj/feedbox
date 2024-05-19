2944161be17b6aae14e766c5e7f40f4e1b304e7c
h11 <git@h11.dev>
2024-05-19 10:20:54 +0800

    wip: infernojs

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -1,11 +1,16 @@
 import commonjs from "@rollup/plugin-commonjs"
 import html from "@rollup/plugin-html"
 import resolve from "@rollup/plugin-node-resolve"
+import replace from "@rollup/plugin-replace"
 import terser from "@rollup/plugin-terser"
 import typescript from "@rollup/plugin-typescript"
 import transformInferno from "ts-plugin-inferno"
 
 const prod = process.env.NODE_ENV === "production"
+const env = Object.entries(process.env).reduce((acc, [k, v]) => {
+	acc["process.env." + k] = JSON.stringify(v)
+	return acc
+}, {})
 
 export default {
 	input: {
@@ -19,14 +24,24 @@ export default {
 		generatedCode: "es2015",
 	},
 	plugins: [
-		resolve(),
+		resolve({
+			mainFields: prod
+				? ["module", "main"]
+				: ["dev:module", "module", "main"],
+		}),
+		// commonjs(),
+		replace({
+			objectGuards: true,
+			preventAssignment: true,
+			values: { ...env },
+		}),
 		typescript({
 			transformers: {
 				after: [transformInferno.default()],
 			},
 		}),
-		commonjs(),
-		html({ template: generateHtml }),
+		html({ fileName: "index.html", template: generateHtml }),
+		html({ fileName: "index.html.json", template: generateHtmlJson }),
 		prod && terser(),
 	],
 }
@@ -58,4 +73,27 @@ function generateHtml({ files }) {
 		"</html>",
 	]
 	return tmpl.join("\n")
+}
+
+function generateHtmlJson({ files }) {
+	const styles = (files.css || []).map(
+		({ fileName }) => `<${fileName}>; rel=preload; as=style; crossorigin`,
+	)
+
+	const scripts = (files.js || []).map(
+		({ fileName }) => `<${fileName}>; rel=preload; as=script; crossorigin`,
+	)
+
+	return JSON.stringify(
+		{
+			header: [
+				{
+					key: "Link",
+					value: [...scripts, ...styles].join(","),
+				},
+			],
+		},
+		null,
+		4,
+	)
 }
